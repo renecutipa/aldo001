c6d112807b160dd7a1c1d185478bc3df5ce39993
Rene Cutipa <renecutipa@gmail.com>
2020-12-22 13:58:15 -0500

    block

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,230 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('frontend/img/apple-icon.png')}}">
+  <link rel="icon" type="image/png" href="{{ asset('frontend/img/favicon.png')}}">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+    Quinua
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700,200" rel="stylesheet" />
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <!-- CSS Files -->
+  <link href="{{ asset('frontend/css/bootstrap.min.css') }}" rel="stylesheet">
+  <link href="{{ asset('frontend/css/now-ui-kit.css?v=1.3.0') }}" rel="stylesheet">
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="{{ asset('frontend/demo/demo.css') }}" rel="stylesheet">
+</head>
+
+<body class="index-page sidebar-collapse">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg bg-primary fixed-top navbar-transparent " color-on-scroll="100">
+    <div class="container">
+      <div class="navbar-translate">
+        <a class="navbar-brand" href="https://demos.creative-tim.com/now-ui-kit/index.html" rel="tooltip" title="CHENOPODIUM QUINOA" data-placement="bottom" target="_blank">
+          Quinua
+        </a>
+        <button class="navbar-toggler navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-bar top-bar"></span>
+          <span class="navbar-toggler-bar middle-bar"></span>
+          <span class="navbar-toggler-bar bottom-bar"></span>
+        </button>
+      </div>
+
+      <form class="form-inline">              
+              <div class="form-group mx-sm-12">
+                <input type="text" class="form-control form-block" placeholder="" style="background: #FFF;">
+              </div>
+              <button type="submit" class="btn btn-primary btn-round">Buscar</button>
+        </form>
+      <!--div class="collapse navbar-collapse justify-content-end" id="navigation" data-nav-image="{{ asset('frontend/img/blurred-image-1.jpg')}}">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="javascript:void(0)" onclick="scrollToDownload()">
+              <i class="now-ui-icons arrows-1_cloud-download-93"></i>
+              <p>Download</p>
+            </a>
+          </li>
+          <li class="nav-item dropdown">
+            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdownMenuLink1" data-toggle="dropdown">
+              <i class="now-ui-icons design_app"></i>
+              <p>Components</p>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink1">
+              <a class="dropdown-item" href="./index.html">
+                <i class="now-ui-icons business_chart-pie-36"></i> All components
+              </a>
+              <a class="dropdown-item" target="_blank" href="https://demos.creative-tim.com/now-ui-kit/docs/1.0/getting-started/introduction.html">
+                <i class="now-ui-icons design_bullet-list-67"></i> Documentation
+              </a>
+            </div>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link btn btn-neutral" href="https://www.creative-tim.com/product/now-ui-kit-pro" target="_blank">
+              <i class="now-ui-icons arrows-1_share-66"></i>
+              <p>Upgrade to PRO</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="Follow us on Twitter" data-placement="bottom" href="https://twitter.com/CreativeTim" target="_blank">
+              <i class="fab fa-twitter"></i>
+              <p class="d-lg-none d-xl-none">Twitter</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="Like us on Facebook" data-placement="bottom" href="https://www.facebook.com/CreativeTim" target="_blank">
+              <i class="fab fa-facebook-square"></i>
+              <p class="d-lg-none d-xl-none">Facebook</p>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" rel="tooltip" title="Follow us on Instagram" data-placement="bottom" href="https://www.instagram.com/CreativeTimOfficial" target="_blank">
+              <i class="fab fa-instagram"></i>
+              <p class="d-lg-none d-xl-none">Instagram</p>
+            </a>
+          </li>
+        </ul>
+      </div-->
+    </div>
+  </nav>
+  <!-- End Navbar -->
+  <div class="wrapper">
+    <div class="page-header clear-filter" filter-color="orange">
+      <div class="page-header-image" data-parallax="true" style="background-image:url('./frontend/img/header2.jpg');">
+      </div>
+      <!--div class="container">
+        <div class="content-center brand">
+          <img class="n-logo" src="./frontend/img/quinua.png" alt="">
+          <h1 class="h1-seo">QUINUA</h1>
+          <h3>Sistema Semántico para el tratamiento de la información Biotecnológica de granos Andinos</h3>
+        </div>
+      </div-->
+    </div>
+    <div class="main">
+      <!--div class="section section-images">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div class="hero-images-container">
+                <img src="frontend/img/hero-image-1.png" alt="">
+              </div>
+              <div class="hero-images-container-1">
+                <img src="frontend/img/hero-image-2.png" alt="">
+              </div>
+              <div class="hero-images-container-2">
+                <img src="frontend/img/hero-image-3.png" alt="">
+              </div>
+            </div>
+          </div>
+        </div>
+      </div-->
+      
+
+      <div class="section section-basic" id="basic-elements">
+
+        <div class="container">
+              
+        </div>
+
+        <div class="container">
+          <h3 class="title">Ultimos artículos</h3>
+            <div class="row">
+            <div class="col-md-10 ml-auto col-xl-6 mr-auto">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">QUINUA INIA 415 PASANKALLA</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">Autor: Juan Perez</h6>
+                        <p class="card-text">La quinua (Chenopodium quinoa Wild.), es una especie originaria de los andes peruanos, que posee múltiples cualidades...</p>
+                        <a href="#" class="card-link">Leer más...</a>
+                        <a href="#" class="card-link">Más del autor</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-10 ml-auto col-xl-6 mr-auto">
+                <div class="card">
+                    <div class="card-body">
+                        <h4 class="card-title">QUINUA ILLPA INIA</h4>
+                        <h6 class="card-subtitle mb-2 text-muted">Autor: Juan Lopez</h6>
+                        <p class="card-text">INTRODUCCIÓN 
+                        La quinua (Chenopodium quinoa Willd.), es una especie originaria de los andes peruanos...</p>
+                        <a href="#" class="card-link">Leer más...</a>
+                        <a href="#" class="card-link">Más del autor</a>
+                    </div>
+                </div>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+      
+    </div>
+    <!--  End Modal -->
+    <footer class="footer" data-background-color="black">
+      <div class=" container ">
+        <nav>
+          <ul>
+            <li>
+              <a href="https://www.creative-tim.com">
+                Creative Tim
+              </a>
+            </li>
+            <li>
+              <a href="http://presentation.creative-tim.com">
+                About Us
+              </a>
+            </li>
+            <li>
+              <a href="http://blog.creative-tim.com">
+                Blog
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div class="copyright" id="copyright">
+          &copy;
+          <script>
+            document.getElementById('copyright').appendChild(document.createTextNode(new Date().getFullYear()))
+          </script>, Designed by
+          <a href="https://www.invisionapp.com" target="_blank">Invision</a>. Coded by
+          <a href="https://www.creative-tim.com" target="_blank">Creative Tim</a>.
+        </div>
+      </div>
+    </footer>
+  </div>
+  <!--   Core JS Files   -->
+  <script src="{{ asset('frontend/js/core/jquery.min.js')}}" type="text/javascript"></script>
+  <script src="{{ asset('frontend/js/core/popper.min.js')}}" type="text/javascript"></script>
+  <script src="{{ asset('frontend/js/core/bootstrap.min.js')}}" type="text/javascript"></script>
+  <!--  Plugin for Switches, full documentation here: http://www.jque.re/plugins/version3/bootstrap.switch/ -->
+  <script src="{{ asset('frontend/js/plugins/bootstrap-switch.js')}}"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="{{ asset('frontend/js/plugins/nouislider.min.js')}}" type="text/javascript"></script>
+  <!--  Plugin for the DatePicker, full documentation here: https://github.com/uxsolutions/bootstrap-datepicker -->
+  <script src="{{ asset('frontend/js/plugins/bootstrap-datepicker.js')}}" type="text/javascript"></script>
+  <!--  Google Maps Plugin    -->
+  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+  <!-- Control Center for Now Ui Kit: parallax effects, scripts for the example pages etc -->
+  <script src="{{ asset('frontend/js/now-ui-kit.js?v=1.3.0')}}" type="text/javascript"></script>
+  <script>
+    $(document).ready(function() {
+      // the body of this function is in assets/js/now-ui-kit.js
+      nowuiKit.initSliders();
+    });
+
+    function scrollToDownload() {
+
+      if ($('.section-download').length != 0) {
+        $("html, body").animate({
+          scrollTop: $('.section-download').offset().top
+        }, 1000);
+      }
+    }
+  </script>
+</body>
+
+</html>
